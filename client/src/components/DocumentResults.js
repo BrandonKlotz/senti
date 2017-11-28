a55070ee843e56f47cc47a9b6b2ff806bcc81a0c
Joey Parks <josephparks13@gmail.com>
2017-11-28 15:48:28 -0500

    added if statement to mapping function in DocumentResults component.

--- client/src/components/DocumentResults.js
+++ client/src/components/DocumentResults.js
@@ -2,16 +2,41 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class DocumentResults extends Component {
+
   render() {
 
-    const detectedThemes = this.props.displayResults.document_tone.tones.map(tone => 
-      <p>{tone.tone_name}</p>
-      );
+    //The map below will save all the document themes into an accorian element.
+
+    const detectedDocThemes = this.props.displayResults.document_tone.tones.map(function(tone) {
+
+      if(tone.tone_name == "Joy" || tone.tone_name == "Analytical") {
+        return (
+          <div>
+            <button class="accordion">{tone.tone_name}</button>
+              <div class="panel">
+                <p>This is a positive one.</p>
+              </div>
+          </div>
+        )
+      }
+      else {
+        return (
+          <div>
+            <button class="accordion">{tone.tone_name}</button>
+              <div class="panel">
+                <p>This is a negative one.</p>
+              </div>
+          </div>
+        )
+      }
+    });
+
+    //Returns the accordian to the component with a Results Header.
 
     return (
       <div className="DocumentResults">
         <h2>Results</h2>
-        {detectedThemes}
+        {detectedDocThemes}
       </div>
     );
   }
