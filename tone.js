8e77531ed7afe985891e1c844f173a37d3dde289
Brandon Klotz <brandonklotz@icloud.com>
2017-11-29 13:13:40 -0500

    trying to implement advice from watson dev team to get results to display in front-end

--- tone.js
+++ tone.js
@@ -3,6 +3,8 @@ var bodyParser = require("body-parser");
 var express = require('express');
 var router = express();
 
+require('dotenv').config();
+
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
@@ -12,10 +14,10 @@ router.post('/info', function(req, res) {
     var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
     var tone_analyzer = new ToneAnalyzerV3({
-     username: 'yourUsername',
-     password: 'yourPassword',
-     version_date: '2016-05-19'
-     });
+      username: process.env.TONE_ANALYZER_USERNAME,
+      password: process.env.TONE_ANALYZER_PASSWORD,
+      version_date: '2017-11-28'
+    });
 
     tone_analyzer.tone({
         text: text
