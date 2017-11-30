ab82f9c6958ac09ca852eecf43f5765d534e553b
Joey Parks <josephparks13@gmail.com>
2017-11-30 09:24:05 -0500

    Sentence Anaylsis returns divs containing names of detected sentence emotions. Bug returns grouped emotions on sentences that contain more than one emotion.

--- client/src/components/SentenceResults.js
+++ client/src/components/SentenceResults.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { join } from 'lodash';
+import { join, chunk, uniq } from 'lodash';
 
 class SentenceResults extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTone: ""
+    };
+  }
 
   render() {
 
     const sentences = this.props.displayResults.sentences_tone.map(function(sentence) {
 
       return (
-          <div
-            key={sentence.sentence_id}
-            className={join(sentence.tones.map(tone => {
-              return(tone.tone_id)
-            }), " ")}>
-              {sentence.text}
-          </div>)
+        <div
+          key={sentence.sentence_id}
+          className={join(sentence.tones.map(tone => {
+            return(tone.tone_id)
+          }), " ")}>
+            {sentence.text}
+        </div>
+      )
     });
 
+    const buttons = this.props.displayResults.sentences_tone.map(function(sentence) {
+    
+      return (
+        sentence.tones.map(tone => {
+          return(tone.tone_name)
+        })
+      )
+    });
+
+    var buttonsText = buttons.map(function(item) { 
+      return join(item); 
+    });
+
+    var uniqueButtons = uniq(buttonsText);
+
+    var displayButtons = uniqueButtons.map(button => {
+      return (<div className={button+"toggle"}>{button}</div>)
+    })
+
+    console.log(displayButtons);
 
     return (
       <div className="SentenceResults">
         {sentences}
+        {displayButtons}
       </div>
     );
   }
