95ea7a869b617d2bb921de17038f88d6eb112e17
Keenan Barber <keenan.c.barber@gmail.com>
2017-11-30 10:50:04 -0500

    Added Tone Data file with stored information about given tones. Allows program to show information about each returned tone at document level. Included flag and check for negative and positive tones respectively.

--- client/src/components/DocumentResults.js
+++ client/src/components/DocumentResults.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import "font-awesome/css/font-awesome.css";
+import toneData from '../ToneData';
+
 
 class DocumentResults extends Component {
 
@@ -9,36 +12,49 @@ class DocumentResults extends Component {
       open: ""
     };
   }
-
+//  Hello world! I am having an awesome day! I hate you.
   render() {
 
     //if active state = add open class to element
 
     const detectedDocThemes = this.props.displayResults.document_tone.tones.map(tone => {
+      debugger;
 
-      if(tone.tone_name === "Joy") {
+       for(var i=0; i<toneData.length; i++) {
+          var selectedTone = toneData[i];
+
+      console.log(selectedTone.tone);
+
+      if(tone.tone_name === selectedTone.tone) {
+
+        if (selectedTone.positive) {
         return (
             <div  className={`accordion accordion_positive ${tone.tone_name === this.state.open?"open":"collapsed"}`}
                   key={tone.tone_name}
                   onClick={() => this.toggleAccordion(tone.tone_name)}>
+                  <i class="fa fa-check" aria-hidden="true"></i>
                     {tone.tone_name}
-                    <p>This is a positive one.</p>
+                    <p>{tone.tone_name}: {selectedTone.text}</p>
             </div>
         )
       }
+    
       else {
         return (
 
             <div  className={`accordion accordion_positive ${tone.tone_name === this.state.open?"open":"collapsed"}`}
                   key={tone.tone_name}
                   onClick={() => this.toggleAccordion(tone.tone_name)}>
+                  <i class="fa fa-flag" aria-hidden="true"></i>
                     {tone.tone_name}
-                    <p>This is a negative one.</p>
+                    <p>{tone.tone_name}: {selectedTone.text}</p>
             </div>
-
         )
       }
+    }
+  }
     });
+
 
     return (
       <div className="DocumentResults">
