f6bbc8beb246c15fb21b74dcb0ede0b0029d56d2
Keenan Barber <keenan.c.barber@gmail.com>
2017-12-06 09:13:02 -0500

    Added touch functionality for IOS Safari mobile.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,7 @@ import TextAnalyzer from "./TextAnalyzer.js";
 class App extends Component {
     render() {
         return (
-            <div>
+            <div ontouchstart="">
                 <Header />
                 <TextAnalyzer />
             </div>
